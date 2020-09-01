b1fdeba8315cb93a95c1a553023401630144494a
GuilhermeBergamoBR <bergamoguilherme14@gmail.com>
2020-09-01 19:05:29 -0300

    matriz feita

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var vertices = document.getElementById('vertices')
-
-console.log(vertices)
+function returnValue() {
+  var vertices = document.getElementById("vertices").value;
+  var matrizVertices = new Array(parseInt(vertices))
+  
+  for (const indice of matrizVertices) {
+    matrizVertices[indice] =  new Array(parseInt(vertices))
+  }
+  
+  console.log(matrizVertices)
+}
