634c975976c655d466d1c052e28c52af7fd3bd52
GuilhermeBergamoBR <bergamoguilherme14@gmail.com>
2020-09-09 19:09:46 -0300

    rowfdsfs

--- graph.py
+++ graph.py
@@ -1,1 +1,11 @@
-print('taca a mae pra ver se quica')
+def cria_grafo(lista_de_vertices, lista_de_arestas):
+    grafo = {}
+    for vertice in lista_vertices:
+        grafo[vertice] = []
+    for aresta in lista_arestas:
+        grafo[aresta[0]].append(aresta[1])
+    return grafo
+
+lista_vertices = ['a', 'b', 'c', 'd', 'e', 'f']
+lista_arestas = [('a', 'b'), ('b', 'a'), ('b', 'c'), ('c', 'b'), ('c', 'e'), ('e', 'a'), ('e', 'b'), ('e', 'c'), ('e', 'd'), ('d', 'a'), ('d', 'e'), ('e', 'f'), ('f', 'e'), ('b', 'e'), ('a', 'd'), ('a', 'e')]
+grafo = cria_grafo
